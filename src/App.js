586e024317aea7f73d6ff075bb5bf74fd24711a7
Rio <69231309+wolfchapo@users.noreply.github.com>
2021-07-13 11:11:55 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+
+
+const App = () => {
+  const [showAddTask, setShowAddTask] =useState(false)
+  const [tasks, setTasks] = useState([])
+  
+//Add task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000 + 1)
+  const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+}
+
+//delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id
+  ))
+}
+
+//Toggle reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => task.id === id ? 
+    { ...task, reminder: !task.reminder } : task
+    ))
+} 
+
+
+
+
+  return (
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask}
+        />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? ( 
+      <Tasks tasks={tasks} onDelete={deleteTask}
+         onToggle={toggleReminder}
+        /> ):('No Task Available'
+      )}
+    </div>
+  );
+}
+
+
+export default App;
